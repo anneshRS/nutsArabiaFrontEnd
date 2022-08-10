de8b75b88552e8ca5f72962c6cb1c87c8cb63009
anneshRS <annesh@resemblesystems.com>
2022-08-10 13:53:40 +0530

    type error3

--- src/modules/common/components/tags/tags.tsx
+++ src/modules/common/components/tags/tags.tsx
@@ -7,7 +7,7 @@ const Tags: React.FC<ITagsProps> = ({ product }: ITagsProps) => {
     <>
       {product.tag.length !== 0 && (
         <div className="flex flex-row">
-          {JSON.parse(product?.tag).map((t, i) => (
+          {JSON.parse(product?.tag).map((t:any, i:any) => (
             <span
               key={i + 1}
               className="bg-gray-50 mr-2 border-0 text-gray-600 rounded-full inline-flex items-center justify-center px-3 py-1 text-xs font-semibold font-serif mt-2"
