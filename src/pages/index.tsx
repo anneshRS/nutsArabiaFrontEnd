10c97e635d9b0e86e61bc7765db5bbdee9d161dd
anneshRS <annesh@resemblesystems.com>
2022-08-12 16:02:46 +0530

    Slider fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,14 +29,14 @@ const Home: NextPageWithLayout = () => {
       {/* <FeaturedProducts /> */}
       <div className="bg-white">
         <div className="mx-auto py-5 max-w-screen-2xl px-3 sm:px-10">
-          {/* <div className="flex w-full">
+          <div className="flex w-full">
             <div className="flex-shrink-0 xl:pr-6 lg:block w-full lg:w-3/5">
               <MainCarousel />
             </div>
             <div className="w-full hidden lg:flex">
               <OfferCard />
             </div>
-          </div> */}
+          </div>
           <div className="bg-orange-100 px-10 py-6 rounded-lg mt-6 hidden lg:block">
             <Banner />
           </div>
