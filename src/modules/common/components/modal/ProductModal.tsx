4530c957e1569b192afb9438068309896dbdbdc4
Vijayan Raghavan <vijayan@Priyeshs-Mac.local>
2022-08-12 11:05:32 +0530

    product card completed

--- src/modules/common/components/modal/ProductModal.tsx
+++ src/modules/common/components/modal/ProductModal.tsx
@@ -14,11 +14,13 @@ import {
 import { dehydrate, QueryClient, useQuery } from "react-query"
 import { medusaClient } from "@lib/config"
 import { GetStaticProps } from "next"
+import ProductCard from "@modules/products/templates/theme/ProductCard"
 
 export interface IProductModalProps {
   productDetails: any
   modalOpen: any
   setModalOpen: any
+  // productVariant?: any
 }
 const fetchProduct = async (handle: string) => {
   return await medusaClient.products
@@ -28,123 +30,48 @@ const fetchProduct = async (handle: string) => {
 
 const ProductModal: React.FC<IProductModalProps> = ({
   productDetails,
+  // productVariant,
   modalOpen,
   setModalOpen,
 }: IProductModalProps) => {
   // const { handleAddItem, setItem, item } = useAddToCart()
   // const [item, setItem] = useState(1)
-  const {
-    increaseQuantity,
-    addToCart,
-    decreaseQuantity,
-    inStock,
-    variant,
-    quantity,
-  } = useProductActions()
+  // const {
+  //   increaseQuantity,
+  //   addToCart,
+  //   decreaseQuantity,
+  //   inStock,
+  //   variant,
+  //   quantity,
+  // } = useProductActions()
   // console.log(item)
 
-  const { data, isError, isLoading, isSuccess } = useQuery(
-    [`get_product`, productDetails.handle],
-    () => fetchProduct(productDetails.handle),
-    {
-      enabled: productDetails.handle.length > 0,
-      keepPreviousData: true,
-    }
-  )
-  const product = data
-  console.log("queData", product)
+  // const { data, isError, isLoading, isSuccess } = useQuery(
+  //   [`get_product`, productDetails.handle],
+  //   () => fetchProduct(productDetails.handle),
+  //   {
+  //     enabled: productDetails.handle.length > 0,
+  //     keepPreviousData: true,
+  //   }
+  // )
+  // const product = data
+  // console.log("queData", product)
+
+  console.log("first", productDetails)
 
   return (
+    // <MainModal modalOpen={modalOpen} setModalOpen={setModalOpen}>
+    //   {/* {productVariant?.map((variant: any) => {
+    //     ;<ProductCard key={productDetails.id} product={product} />
+    //   })} */}
+    //   hii
+    // </MainModal>
+
     // <ProductProvider product={product}>
+
     <MainModal modalOpen={modalOpen} setModalOpen={setModalOpen}>
       <div className="inline-block overflow-y-auto h-full align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-        <div className="flex flex-col lg:flex-row md:flex-row w-full max-w-4xl overflow-hidden">
-          <Link href={`/product/${productDetails.handle}`} passHref>
-            <div
-              onClick={() => setModalOpen(false)}
-              className="flex-shrink-0 flex items-center justify-center h-auto cursor-pointer"
-            >
-              <Image
-                src={productDetails.thumbnail}
-                width={420}
-                height={420}
-                alt={productDetails.title}
-              />
-            </div>
-          </Link>
-          <div className="w-full flex flex-col p-5 md:p-8 text-left">
-            <div className="mb-2 md:mb-2.5 block -mt-1.5">
-              <Link href={`/product/${productDetails.handle}`} passHref>
-                <h1
-                  onClick={() => setModalOpen(false)}
-                  className="text-heading text-lg md:text-xl lg:text-2xl font-semibold font-serif hover:text-black cursor-pointer"
-                >
-                  {productDetails.title}
-                </h1>
-              </Link>
-              <Stock product={productDetails} />
-              <p className="text-sm leading-6 text-gray-500 md:leading-6">
-                {productDetails.description}
-              </p>
-            </div>
-            <p className="text-sm leading-6 text-gray-500 md:leading-6">
-              {productDetails.description}
-            </p>
-            <div className="flex items-center mt-4">
-              <Price product={productDetails} />
-            </div>
-
-            <div className="flex items-center mt-4">
-              <div className="flex items-center justify-between space-s-3 sm:space-s-4 w-full">
-                <div className="group flex items-center justify-between rounded-md overflow-hidden flex-shrink-0 border h-11 md:h-12 border-gray-300">
-                  <button
-                    // onClick={() => setItem(item - 1)}
-                    // onClick={decreaseQuantity}
-                    // disabled={item === 1}
-                    // disabled={quantity === 1}
-                    className="flex items-center justify-center flex-shrink-0 h-full transition ease-in-out duration-300 focus:outline-none w-8 md:w-12 text-heading border-e border-gray-300 hover:text-gray-500"
-                  >
-                    <span className="text-dark text-base">
-                      <FiMinus />
-                    </span>
-                  </button>
-                  <p className="font-semibold flex items-center justify-center h-full  transition-colors duration-250 ease-in-out cursor-default flex-shrink-0 text-base text-heading w-8  md:w-20 xl:w-24">
-                    {/* {quantity} */}
-                  </p>
-                  <button
-                    // onClick={() => setItem(item + 1)}
-                    // onClick={increaseQuantity}
-                    // disabled={
-                    // product.quantity < item || product.quantity === item
-                    // productDetails.quantity < quantity ||
-                    // productDetails.quantity === quantity
-                    // }
-                    className="flex items-center justify-center h-full flex-shrink-0 transition ease-in-out duration-300 focus:outline-none w-8 md:w-12 text-heading border-s border-gray-300 hover:text-gray-500"
-                  >
-                    <span className="text-dark text-base">
-                      <FiPlus />
-                    </span>
-                  </button>
-                </div>
-                <button
-                  // onClick={() => handleAddItem(product)}
-                  // onClick={addToCart}
-                  // disabled={productDetails.quantity < 1}
-                  className="text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-serif text-center justify-center border-0 border-transparent rounded-md focus-visible:outline-none focus:outline-none text-white px-4 ml-4 md:px-6 lg:px-8 py-4 md:py-3.5 lg:py-4 hover:text-white bg-emerald-500 hover:bg-emerald-600 w-full h-12"
-                >
-                  Add To Cart
-                </button>
-              </div>
-            </div>
-            <div className="flex flex-col mt-4">
-              <span className="font-serif font-semibold py-1 text-sm d-block">
-                <span className="text-gray-700">Category:</span>{" "}
-                <span className="text-gray-500">{productDetails.children}</span>
-              </span>
-              {/* <Tags product={product} /> */}
-            </div>
-          </div>
-        </div>
+        {productDetails[0].id}
       </div>
     </MainModal>
     // </ProductProvider>
--- src/modules/products/templates/theme/ProductCard.tsx
+++ src/modules/products/templates/theme/ProductCard.tsx
@@ -3,10 +3,11 @@ import { ProductProvider } from "@lib/context/product-context"
 import ProductModal from "@modules/common/components/modal/ProductModal"
 import Price from "@modules/common/components/price/Price"
 import Image from "next/image"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { IoBagAddSharp, IoAdd, IoRemove } from "react-icons/io5"
 import { useQuery } from "react-query"
 import { formatVariantPrice, useCart } from "medusa-react"
+import { ChevronDownIcon } from "@heroicons/react/outline"
 
 export interface IProductCardProps {
   product: any
@@ -15,9 +16,11 @@ export interface IProductCardProps {
 const ProductCard: React.FC<IProductCardProps> = ({
   product,
 }: IProductCardProps) => {
-  
   const [modalOpen, setModalOpen] = useState(false)
   const { cart } = useCart()
+  const [variant, setVariant] = useState([])
+  const [currentDisplayableVariant, setCurrentDisplayableVariant] =
+    useState<any>({})
 
   const fetchProduct = async (handle: string) => {
     return await medusaClient.products
@@ -34,22 +37,33 @@ const ProductCard: React.FC<IProductCardProps> = ({
     }
   )
 
+  useEffect(() => {
+    if (data && Object.keys(data).length > 0) {
+      let variant: any = data.variants
+      setVariant(variant)
+      setCurrentDisplayableVariant(variant[0])
+    }
+  }, [data])
+
+  console.log("vari", variant)
+
   console.log("Newdata", data)
   // console.log("quan", product.title)
   return (
     <>
       {data && Object.keys(data).length > 0 && (
-        <ProductProvider product={data}>
+        <>
           <ProductModal
             modalOpen={modalOpen}
             setModalOpen={setModalOpen}
-            productDetails={product}
+            productDetails={variant}
+            // productVariant={variant}
           />
 
-          <div className="group box-border overflow-hidden flex rounded-md shadow-sm pe-0 flex-col items-center bg-white relative">
+          <div className="group box-border overflow-hidden flex rounded-md shadow-sm pe-0 my-2 flex-col items-center bg-white relative">
             <div
-              onClick={() => setModalOpen(!modalOpen)}
-              className="relative flex justify-center w-full cursor-pointer"
+              // onClick={() => setModalOpen(!modalOpen)}
+              className="relative flex justify-center w-full cursor-pointer pt-2"
             >
               {product.quantity <= 0 && (
                 <span className="absolute inline-flex items-center justify-center px-2 py-1 bg-red-100 text-red-600 border-0 rounded-full text-xs font-semibold font-serif z-10 left-4 top-4">
@@ -66,21 +80,52 @@ const ProductCard: React.FC<IProductCardProps> = ({
                 className="object-cover transition duration-150 ease-linear transform group-hover:scale-105"
               />
             </div>
-            <div className="w-full px-3 lg:px-4 pb-4 overflow-hidden">
+            <div className="w-full px-3 lg:px-4 py-2 overflow-hidden">
               <div className="relative mb-1">
-                <span className="text-gray-400 font-medium text-xs d-block mb-1">
-                  {/* product.unit */ "each"}
-                </span>
+                {/* <span className="text-gray-400 font-medium text-xs d-block mb-1">
+                each
+                </span> */}
                 <h2 className="text-heading truncate mb-0 block text-sm font-medium text-gray-600">
                   <span className="line-clamp-2">{product.title}</span>
                 </h2>
               </div>
-              <div className="flex justify-between items-center text-heading text-sm sm:text-base space-s-2 md:text-base lg:text-xl">
+
+              {/* <div className="flex justify-between items-center text-heading text-sm sm:text-base space-s-2 md:text-base lg:text-xl">
                 <Price product={product} card={true} />
+              </div> */}
+            </div>
+
+            <div className="flex w-full justify-between items-center px-3 lg:px-4 pb-2">
+              <div
+                className="flex justify-content items-center border border-gray-200 rounded shadow hover:border-emerald-500 px-2"
+                onClick={() => setModalOpen(!modalOpen)}
+              >
+                <span className="h-9 w-auto flex items-center justify-center transition-all">
+                  {currentDisplayableVariant &&
+                    Object.keys(currentDisplayableVariant).length > 0 &&
+                    currentDisplayableVariant.options &&
+                    currentDisplayableVariant.options.length > 0 &&
+                    currentDisplayableVariant.options[0]?.value}
+                </span>
+
+                <ChevronDownIcon
+                  className="ml-1 h-3 w-3 group-hover:text-emerald-600"
+                  aria-hidden="true"
+                />
               </div>
+              <button
+                // onClick={() => handleAddItem(product)}
+                // disabled={product.quantity < 1}
+                aria-label="cart"
+                className="h-9 w-9 flex items-center justify-center border border-gray-200 rounded text-emerald-500 hover:border-emerald-500 hover:bg-emerald-500 hover:text-white transition-all shadow"
+              >
+                <span className="text-xl">
+                  <IoBagAddSharp />
+                </span>
+              </button>
             </div>
           </div>
-        </ProductProvider>
+        </>
       )}
     </>
   )
