3e0bf18010949deae77e26534be558910b79fa82
anneshRS <annesh@resemblesystems.com>
2022-08-24 21:46:31 +0530

    adding hosted backend

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,7 +3,7 @@ import { QueryClient } from "react-query"
 
 // Defaults to standard port for Medusa server
 let MEDUSA_BACKEND_URL =
-  "http://localhost:9000" /*   "https://nutsarabia-backend-staging-tiz3i.ondigitalocean.app" */
+  /* "http://localhost:9000" */   "https://nutsarabia-backend-staging-tiz3i.ondigitalocean.app"
 
 if (process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL) {
   MEDUSA_BACKEND_URL = process.env.NEXT_PUBLIC_MEDUSA_BACKEND_URL
